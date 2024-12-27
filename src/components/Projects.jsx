27b02abe478536dae9ed7ef0a1d3945410bb8e13
bijoy <bijoycse24@gmail.com>
2024-12-27 23:48:03 +0600

    testimonial added

---
+++ src/components/Projects.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import {assets, projectsData} from '../assets/assets'
+function Projects() {
+  const [currentIndex,setCurrentIndex] =useState(0);
+  const [cardtoShow,setCardtoshow] =useState(1);
+  const nextprojects =()=>{
+    setCurrentIndex(previousIndex=>previousIndex + 1) % projectsData.length;
+  }
+  const PrevProjects =()=>{
+    setCurrentIndex(previousIndex =>previousIndex === 0 ? projectsData.length -1 : previousIndex -1)
+  }
+  useEffect(()=>{
+    const updateCardtoShow =()=>{
+      if(window.innerWidth >= 1024){
+        setCardtoshow(projectsData.length)
+      }else{
+        setCardtoshow(1)
+      };
+    }
+      updateCardtoShow();
+      window.addEventListener('resize', updateCardtoShow)
+      return ()=>{
+        window.removeEventListener('resize',updateCardtoShow)
+      }
+    
+    
+  },[])
+  return (
+    <div className='container mx-auto px-6 py-4 md:px-20 lg:px-32 my-20 w-full overflow-hidden'>
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 font-light under'>Completed</span></h1>
+        <p className='max-w-80 mx-auto text-center text-gray-500 mb-8'>Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
+
+        {/* slider button */}
+        <div className='flex justify-end items-center mb-8'>
+          <button onClick={PrevProjects} className='bg-gray-200 p-3 mr-2 rounded'>
+            <img src={assets.left_arrow} alt="Previous" aria-label='Previous project' />
+          </button>
+          <button onClick={nextprojects} className='bg-gray-200 p-3 mr-2 rounded'>
+            <img src={assets.right_arrow} alt="Next" aria-label='Next project' />
+          </button>
+        </div>
+        {/* slider button */}
+        {/* project slider container */}
+        <div className='overflow-hidden'>
+          <div className='flex gap-8 transition-transform duration-500 ease-in-out'
+          style={{transform:`translateX( -${(currentIndex*100)/ cardtoShow}%)`}}
+          >
+            {
+              projectsData.map((project,index)=>(
+                <div key={index} className='w-full sm:w-1/4 flex-shrink-0 relative'>
+                  <img className='w-full h-auto mb-8' src={project.image} alt={project.title} />
+                  <div className='absolute left-0 right-0 bottom-5 flex justify-center'>
+                      <div className='inline-block bg-white w-3/4 px-4 py-2 shadow-md'>
+                        <h2 className='text-xl font-semibold text-gray-800'>{project.title}</h2>
+                        <p className='text-gray-500 text-sm'>{project.price} <span>|</span> {project.location} </p>
+                      </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { assets, testimonialsData } from '../assets/assets'
+function Testimonials() {
+  return (
+    <div className='container mx-auto w-full py-10 lg:px-32 overflow-hidden' id='testimonials'> 
+        <h1 className='text-2xl text-center md:text-4xl mb-2 font-bold'>Customer <span className='underline underline-offset-4 decoration-1 under font-light'>Testimonials</span></h1>
+        <p className='max-w-80 text-gray-400 mx-auto text-center mb-12'>Real Stories from Thos Who found Home with us</p>
+        <div className='flex justify-center gap-10 flex-wrap px-10 py-10'>
+            {
+                testimonialsData.map((testimonial,index)=>(
+                    <div className=' w-full sm:max-w-[340px] border shadow-lg rounded px-8 py-12 text-center' key={index}>
+                        <img className='w-20 h-20 rounded-full mx-auto mb-4 text-center' src={testimonial.image} alt="" />
+                        <h2 className='text-xl text-gray-700 font-medium '>{testimonial.name}</h2>
+                        <p className='text-gray-500 mb-4 text-sm'>{testimonial.title}</p>
+                        <div className='flex justify-center gap-1 text-red-500 mb-4'>
+                            {Array.from({length:testimonial.rating},(item,index)=>(
+                                <img key={index} src={assets.star_icon}/>
+                            ))}
+                        </div>
+                        <p className='text-gray-600'>{testimonial.text}</p>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Testimonials
